a7bb28a16ee592ba9507da1b98a2e4a6998e6c42
8389899 <114801384+8389899@users.noreply.github.com>
2023-05-20 08:37:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+EIP=${EIP:-'162.159.195.17:8854'}
 (
 while ! warp-cli --accept-tos register; do
 	sleep 1
@@ -11,7 +11,8 @@ warp-cli --accept-tos set-proxy-port 40001
 if [ "$LICENSE" != "" ]; then
 	warp-cli --accept-tos set-license $LICENSE
 fi
-
+warp-cli lear-custom-endpoint
+warp-cli set-custom-endpoint $EIP
 warp-cli --accept-tos connect
 socat TCP-LISTEN:40000,fork TCP:localhost:40001  # socat is used to redirect traffic from 40000 to 40001
 ) &
